9dff8d336e0abbc045834c727ba9487486742c68
Cristian Martinez <cristianandres92@gmail.com>
2020-02-24 20:21:58 -0500

    working with context of this

--- assets/index.js
+++ assets/index.js
@@ -5,7 +5,8 @@ const video = document.querySelector("video");
 const playButton = document.getElementById("playButton");
 const muteButton = document.getElementById("muteButton");
 
-const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] });
+// const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] });
+const player = new MediaPlayer({ el: video, plugins: [] });
 
 playButton.onclick = () => player.togglePlay();
 muteButton.onclick = () => player.toggleMute();
