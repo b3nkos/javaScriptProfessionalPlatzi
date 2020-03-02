091c6474abb7612d13c8274de5de6b8134e85a40
Cristian Martinez <cristianandres92@gmail.com>
2020-03-02 11:57:47 -0500

    working with typescript

---
+++ exercises/typescript/index.ts
@@ -0,0 +1,11 @@
+console.log("hello typescript");
+
+enum Color {
+  Red = "Red",
+  Gree = "Gree",
+  Blue = "Blue",
+  Yellow = "Yellow"
+}
+
+const favoriteColor: Color = Color.Blue;
+console.log(`My favorite color is: ${favoriteColor}`);
