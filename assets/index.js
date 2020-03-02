38c8c1819bb241c05bb9995eb582fd8b9427c7f8
Cristian Martinez <cristianandres92@gmail.com>
2020-03-02 11:57:28 -0500

    working with service worker to offline works

--- assets/index.js
+++ assets/index.js
@@ -10,3 +10,11 @@ const player = new MediaPlayer({ el: video, plugins: [new AutoPlay(), new AutoPa
 
 playButton.onclick = () => player.togglePlay();
 muteButton.onclick = () => player.toggleMute();
+
+if('serviceWorker' in navigator) {
+  navigator.serviceWorker
+  .register('/serviceWorker.js')
+  .catch(error => {
+    console.log(error.message);
+  });
+}
