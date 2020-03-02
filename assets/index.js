c1f56aa010c8d53f8e6ce16b5eb3ea5882a642e8
Cristian Martinez <cristianandres92@gmail.com>
2020-03-01 23:52:20 -0500

    media player plugin pause added using intersection observer

--- assets/index.js
+++ assets/index.js
@@ -1,11 +1,12 @@
 import MediaPlayer from './MediaPlayer.js';
 import AutoPlay from './plugins/AutoPlay.js';
+import AutoPause from './plugins/AutoPause.js';
 
 const video = document.querySelector("video");
 const playButton = document.getElementById("playButton");
 const muteButton = document.getElementById("muteButton");
 
-const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] });
+const player = new MediaPlayer({ el: video, plugins: [new AutoPlay(), new AutoPause()] });
 
 playButton.onclick = () => player.togglePlay();
 muteButton.onclick = () => player.toggleMute();
