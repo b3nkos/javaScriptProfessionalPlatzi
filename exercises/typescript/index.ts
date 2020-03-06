8c3ed4c74e5b823fb7168fae4e503908be69c062
Cristian Martinez <cristianandres92@gmail.com>
2020-03-06 02:14:56 -0500

    working with functions and interfaces

--- exercises/typescript/index.ts
+++ exercises/typescript/index.ts
@@ -9,3 +9,42 @@ enum Color {
 
 const favoriteColor: Color = Color.Blue;
 console.log(`My favorite color is: ${favoriteColor}`);
+
+//Functions
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+console.log(`the sum is: ${add(3, 4)}`);
+
+function fullName(firstName: string, lastName: string = 'Smith'): string {
+  return `${firstName} ${lastName}`;
+}
+
+const chris = fullName('Chris', 'Mark');
+console.log(chris);
+
+//Interfaces
+interface Rectangle {
+  with: number,
+  height: number,
+  color?: Color
+}
+
+const rectangle: Rectangle = {
+  with: 4,
+  height: 9,
+  color: Color.Blue
+}
+
+function area(rectangle: Rectangle) {
+  return rectangle.height * rectangle.with;
+}
+
+console.log(area(rectangle));
+
+rectangle.toString = function() {
+  return this.color ? `A rectangle with color \"${this.color}\"` : `A rectangle`;
+}
+
+console.log(rectangle.toString());
